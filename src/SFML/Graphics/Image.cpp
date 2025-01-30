0c9e2834b214070421330cd992d5f56e5202612a
FRex <blazej.roszkowski@gmail.com>
2025-01-30 18:39:20 +0100

    x

--- src/SFML/Graphics/Image.cpp
+++ src/SFML/Graphics/Image.cpp
@@ -37,8 +37,11 @@
 #endif
 
 #define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_STATIC
 #include <stb_image.h>
+
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_STATIC
 #include <stb_image_write.h>
 
 #include <algorithm>
